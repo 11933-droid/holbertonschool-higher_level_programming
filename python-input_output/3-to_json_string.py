da43e3e6cc353ff3ceb843cbebb79bf8c0dd1407
root <root@ip-10-42-93-138.ec2.internal>
2025-12-27 08:22:29 +0000

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This function is for writing to a file
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    This function is for writing to a file
+    """
+    jsonfied_object = json.dumps(my_obj)
+    return jsonfied_object
